06c9afdbeb2934813c6b6add46707c25fc4388cd
enzo-viguier <enzoviguier@outlook.fr>
2024-04-11 13:44:32 +0200

    Ajout des deux microservice à développer

---
+++ ms-rating/src/main/java/fr/enzoviguier/msrating/MsRatingApplication.java
@@ -0,0 +1,13 @@
+package fr.enzoviguier.msrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRatingApplication.class, args);
+	}
+
+}
---
+++ ms-user/src/test/java/fr/enzoviguier/msuser/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.enzoviguier.msuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
